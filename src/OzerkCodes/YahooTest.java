7eba406e91ee9aa476a719c53663f577e7e8d1a4
ozerknet <ozerknet@gmail.com>
2022-05-08 00:31:58 +0100

    YahooTest.java for Accept Button

---
+++ src/OzerkCodes/YahooTest.java
@@ -0,0 +1,15 @@
+package OzerkCodes;
+
+public class YahooTest {
+    public static void main(String[] args) {
+        /* WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://yahoo.com");
+
+        driver.findElement(By.xpath("//*[@id=\"consent-page\"]/div/div/div/form/div[2]/div[2]/button")).click();
+
+        */
+
+    }
+}
