f11a269dbb5f6a03a3cf85542025b34ffd87855a
mustafaornek32 <99423578+mustafaornek32@users.noreply.github.com>
2022-05-09 21:40:58 +0100

    Mustafa first commit

---
+++ src/MustafaOrnek/MustafaOrnek.java
@@ -0,0 +1,9 @@
+package MustafaOrnek;
+
+public class MustafaOrnek {
+
+    public static void main(String[] args) {
+
+        System.out.println("Isparta is one of the most beautiful city in Turkey");
+    }
+}
